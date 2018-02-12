0157605745d51755b44c52cfd826e4ac75a238cb
fanrongzeng <zengfanrong@icarbonx.com>
2018-02-12 12:58:16 +0800

    增加中英文判断

--- splitargs.py
+++ splitargs.py
@@ -3,11 +3,25 @@ import sys
 import os
 import string
 import time
+import re
 from workflow import Workflow3
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+zh_pattern = re.compile(u'[\u4e00-\u9fa5]+')
+
+def contain_zh(word):
+    '''
+    判断传入字符串是否包含中文
+    :param word: 待判断字符串
+    :return: True:包含中文  False:不包含中文
+    '''
+    word = word.decode()
+    global zh_pattern
+    match = zh_pattern.search(word)
+
+    return match
 
 def getargs(wf):
     query = sys.argv[1]
@@ -26,11 +40,17 @@ def getargs(wf):
         sys.stdout.write(query[0].strip())
     elif part == 1:
         # 翻过的结果
-        englishString = query[1]
+        isChinese = contain_zh(query[1])
+        
+        if isChinese:
+            chineseString = query[1]
+            englishString = query[0]
+        else:
+            chineseString = query[0]
+            englishString = query[1]
+
         englishString = englishString.strip().title()
         englishString = englishString.replace(" ","")
-
-        chineseString = query[0]
 
         timeString = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 
