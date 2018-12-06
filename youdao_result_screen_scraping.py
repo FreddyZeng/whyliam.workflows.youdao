35182d021eb6ec78030913691951a7c38ccd3005
Tc8891 <8891ios@8891iOS.local>
2018-12-06 14:04:34 +0800

    截圖翻譯，拼接翻譯結果。不換行

---
+++ youdao_result_screen_scraping.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from workflow import web
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+YOUDAO_DEFAULT_KEYFROM = ('whyliam-wf-1', 'whyliam-wf-2', 'whyliam-wf-3',
+                          'whyliam-wf-4', 'whyliam-wf-5', 'whyliam-wf-6',
+                          'whyliam-wf-7', 'whyliam-wf-8', 'whyliam-wf-9',
+                          'whyliam-wf-10', 'whyliam-wf-11')
+
+YOUDAO_DEFAULT_KEY = (2002493135, 2002493136, 2002493137,
+                      2002493138, 2002493139, 2002493140,
+                      2002493141, 2002493142, 2002493143,
+                      1947745089, 1947745090)
+
+ERRORCODE_DICT = {
+    "50": "有道翻译的API Key错误",
+    "101": "缺少必填的参数，出现这个情况还可能是et的值和实际加密方式不对应",
+    "102": "不支持的语言类型", "103": "翻译文本过长",
+    "104": "不支持的API类型", "105": "不支持的签名类型",
+    "106": "不支持的响应类型", "107": "不支持的传输加密类型",
+    "108": "appKey无效（ 注意不是应用密钥）",
+    "109": "batchLog格式不正确",
+    "110": "无相关服务的有效实例",
+    "111": "开发者账号无效，可能是账号为欠费状态",
+    "201": "解密失败，可能为DES,BASE64,URLDecode的错误",
+    "202": "签名检验失败", "203": "访问IP地址不在可访问IP列表",
+    "301": "辞典查询失败", "302": "翻译查询失败",
+    "303": "服务端的其它异常",
+    "401": "账户已经欠费停",
+    "500": "有道翻译的API Key使用频率过高"
+}
+
+ICON_DEFAULT = 'icon.png'
+ICON_PHONETIC = 'icon_phonetic.png'
+ICON_BASIC = 'icon_basic.png'
+ICON_UPDATE = 'icon_update.png'
+ICON_ERROR = 'icon_error.png'
+
+
+def set_youdao_url(query):
+    # 构建有道翻译URL
+    import os
+    import random
+
+    zhiyun_id = os.getenv('zhiyun_id', '').strip()
+    zhiyun_key = os.getenv('zhiyun_key', '').strip()
+    if zhiyun_id and zhiyun_key:
+        url = set_youdao_new_url_from(query, zhiyun_id, zhiyun_key)
+    else:
+        youdao_keyfrom = os.getenv('youdao_keyfrom', '').strip()
+        youdao_key = os.getenv('youdao_key', '').strip()
+        if not youdao_keyfrom or not youdao_key:
+            i = random.randrange(0, 11, 1)
+            youdao_keyfrom = YOUDAO_DEFAULT_KEYFROM[i]
+            youdao_key = YOUDAO_DEFAULT_KEY[i]
+        url = set_youdao_old_url_from(query, youdao_keyfrom, youdao_key)
+    return url
+
+
+def set_youdao_old_url_from(query, youdao_keyfrom, youdao_key):
+    import urllib
+
+    query = urllib.quote(str(query))
+    url = 'http://fanyi.youdao.com/openapi.do?' + \
+        'keyfrom=' + str(youdao_keyfrom) + \
+        '&key=' + str(youdao_key) + \
+        '&type=data&doctype=json&version=1.1&q=' + query
+    return url
+
+
+def set_youdao_new_url_from(query, zhiyun_id, zhiyun_key):
+    import urllib
+    import hashlib
+    import uuid
+
+    salt = uuid.uuid4().hex
+    sign = hashlib.md5(zhiyun_id + query + salt + zhiyun_key).hexdigest()
+    query = urllib.quote(str(query))
+
+    url = 'http://openapi.youdao.com/api?appKey=' + str(zhiyun_id) + \
+        '&salt=' + str(salt) + \
+        '&sign=' + str(sign) + \
+        '&q=' + query
+
+    return url
+
+
+def get_web_data(query):
+    # 获取翻译数据
+    url = set_youdao_url(query)
+    try:
+        rt = web.get(url).json()
+        return rt
+    except:
+        rt = {}
+        rt['errorCode'] = "500"
+        return rt
+    else:
+        rt = {}
+        rt['errorCode'] = "500"
+        return rt
+
+
+def check_English(query):
+    # 检查英文翻译
+    import re
+
+    if re.search(ur"[\u4e00-\u9fa5]+", query):
+        return False
+    return True
+
+
+def get_translation(query, isEnglish, rt):
+    # 翻译结果
+    subtitle = '翻译结果'
+    translations = rt["translation"]
+    res = ''
+    for title in translations:
+        res = res + "\n" + title
+    arg = '有道' + "\n\n" + query + "\n\n" + res
+    print arg
+
+def get_phonetic(query, isEnglish, rt):
+    # 发音
+    if u'basic' in rt.keys():
+        if rt["basic"].get("phonetic"):
+            title = ""
+            if rt["basic"].get("us-phonetic"):
+                title += ("[美: " + rt["basic"]["us-phonetic"] + "] ")
+            if rt["basic"].get("uk-phonetic"):
+                title += ("[英: " + rt["basic"]["uk-phonetic"] + "] ")
+            title = title if title else "[" + rt["basic"]["phonetic"] + "]"
+            subtitle = '有道发音'
+            arg = [query, title, query, '', ''] if isEnglish else [
+                query, title, '', query, '']
+            arg = '$%'.join(arg)
+
+
+def get_explains(query, isEnglish, rt):
+    # 简明释意
+    if u'basic' in rt.keys():
+        for i in range(len(rt["basic"]["explains"])):
+            title = rt["basic"]["explains"][i]
+            subtitle = '简明释意'
+            arg = [query, title, query, '', ''] if isEnglish else [
+                query, title, '', title, '']
+            arg = '$%'.join(arg)
+
+
+def get_web_translation(query, isEnglish, rt):
+  # 网络翻译
+    if u'web' in rt.keys():
+        for i in range(len(rt["web"])):
+            titles = rt["web"][i]["value"]
+            for title in titles:
+                subtitle = '网络翻译: ' + rt["web"][i]["key"]
+
+                if isEnglish:
+                    key = ''.join(rt["web"][i]["key"])
+                    arg = [query, title, key, '', '']
+                else:
+                    value = ' '.join(rt["web"][i]["value"])
+                    arg = [query, title, title, '', '']
+
+                arg = '$%'.join(arg)
+
+
+if __name__ == '__main__':
+    query = sys.argv[1].strip().replace("\\", "")
+    if not isinstance(query, unicode):
+        query = query.decode('utf8')
+
+    rt = get_web_data(query)
+    errorCode = str(rt.get("errorCode"))
+
+    if ERRORCODE_DICT.has_key(errorCode):
+        print("翻译错误")
+    elif errorCode == "0":
+        isEnglish = check_English(query)
+        get_translation(query, isEnglish, rt)
+#        get_phonetic(query, isEnglish, rt)
+#        get_explains(query, isEnglish, rt)
+#        get_web_translation(query, isEnglish, rt)
